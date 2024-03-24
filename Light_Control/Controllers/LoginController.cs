945bb4a36f4c347386685e49958ba6074bb73e89
JonathanFLF <ferney5585@gmail.com>
2024-03-23 21:13:08 -0500

    fff

---
+++ Light_Control/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Light_Control.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Light_Control/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Light_Control.Models
+{
+    public class LoginModel : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
